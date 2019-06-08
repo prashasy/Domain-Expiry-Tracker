8b502cb44bea2fcf95e4193f3b9d764f5b41f7e6
prashasy <prashasyashok@gmail.com>
2019-06-08 22:56:07 +0530

    added button for subdomain scan

--- display.py
+++ display.py
@@ -5,13 +5,13 @@ def show():
 	con=sql.connect("domain_data.db")
 	cur=con.cursor()
 	res=cur.execute("select * from domains ORDER BY tte ASC",())
-	html="<table><th>Serial</th><th>Domain</th><th>Expiry Date</th><th>Days Left</th>"
+	html="<table><th>Serial</th><th>Domain</th><th>Expiry Date</th><th>Days Left</th><th>Scan</th>"
 	for each in res:
 		html=html+"<tr>"
 		for every in each:
 			html=html+"<td>"+str(every)+"</td>"
 			print(every)
-		html=html+"</tr>"
+		html=html+"<td><button id='run_subdomain_scan'>Scan Sub Domains</button></td></tr>"
 	html=html+"</table>"
 	print(html)
 	return html
