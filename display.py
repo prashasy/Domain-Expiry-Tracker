2e4c018cde9510db123ff07faf826bd95d9159f0
prashasy <prashasyashok@gmail.com>
2019-09-19 13:17:37 +0530

    minor changes

--- display.py
+++ display.py
@@ -4,14 +4,14 @@ import sqlite3 as sql
 def show():
 	con=sql.connect("domain_data.db")
 	cur=con.cursor()
-	res=cur.execute("select * from domains ORDER BY tte ASC",())
+	res=cur.execute("select * from domains",())
 	html="<table><th>Serial</th><th>Domain</th><th>Expiry Date</th><th>Days Left</th><th>Scan</th>"
 	for each in res:
 		html=html+"<tr>"
 		for every in each:
 			html=html+"<td>"+str(every)+"</td>"
 			print(every)
-		html=html+"<td><button id='run_subdomain_scan'>Scan Sub Domains</button></td></tr>"
+		html=html+"<td><button id='"+str(each[1])+"'>Scan Sub Domains</button></td></tr>"
 	html=html+"</table>"
 	print(html)
 	return html
